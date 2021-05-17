76dd2ed96a2e6cbe0e6b702295fe148e44c6ff08
yihu-lv <1720427782@qq.com>
2021-05-17 10:13:23 +0800

    Nav组件，整体结构

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,65 @@
-import React from 'react';
+import Nav from 'components/Nav';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display:flex;
+  flex-direction:column;
+`;
+const Main =  styled.div`
+  flex-grow: 1;
+  overflow:hidden;
+`;
+
 
 function App() {
   return (
-    <div>
-      111111
-    </div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags />
+            </Route>
+            <Route path="/money">
+              <Money />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            <Redirect exact from="/" to="/money"/>
+            <Route path="*">
+              <NoMatch />
+            </Route>
+          </Switch>
+        </Main>
+        <Nav />
+      </Wrapper>
+    </Router>
   );
 }
 
-export default App;
+function Tags() {
+  return <h2>tags</h2>;
+}
+
+function Money() {
+  return <h2>money</h2>;
+}
+
+function Statistics() {
+  return <h2>statistics</h2>;
+}
+
+function NoMatch() {
+  return <h2>输入地址错误</h2>
+}
+
+export default App
