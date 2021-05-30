caffe55da95fb41f9cc207d0a508c1b141c15dbf
yihu-lv <1720427782@qq.com>
2021-05-30 15:57:35 +0800

    修改自动化部署

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/env bash
-
-yarn build &&
-cd build &&
-git init &&
-git add . &&
-git commit -m 'deploy' &&
-git remote add origin git@github.com:ertigers/HuHuLog-react-website.git &&
-git push -u origin master -f
-cd -
-
-
 rm -rf dist &&
 yarn build &&
 cd dist &&
